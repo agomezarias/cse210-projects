69f1c32ab860876c02d4884072dd1d46c7124676
agomezarias <alberto_gomez_arias@yahoo.com>
2023-04-22 23:35:46 -0500

    completed csharp prep 1 and csharp prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percentageFromUser = int.Parse(percentage);
+
+        string letter = "";
+
+        if (percentageFromUser >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentageFromUser >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentageFromUser >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentageFromUser >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (percentageFromUser >= 90 || percentageFromUser < 60)
+        {
+            Console.WriteLine($"You have an: {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"You have a: {letter}");
+        }
+
+        if (percentageFromUser >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Please continue to turn in assignments to improve your grade.");
+        }
     }
 }
