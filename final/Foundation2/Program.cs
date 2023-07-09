8a1c1a4dd0e093a424a202b795f63dd51c91c6fa
agomezarias <alberto_gomez_arias@yahoo.com>
2023-07-09 00:37:12 -0500

    Foundation 2 almost completed

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        List<Product> _products = new List<Product>();
+        new Product("Cheese", "CH3323", 2.99, 2);
+
+        // Testing 
     }
 }
